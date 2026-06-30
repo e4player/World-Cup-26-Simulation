333e80ddf80abe3ffba25591498d56d3e719e10f
e4player <111000957+e4player@users.noreply.github.com>
2026-06-30 18:08:18 -0400

    generalized code for any two teams

--- one_match_simulation.py
+++ one_match_simulation.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-teams = { "France" : 2120, "England" :2100, "Portugal" : 2060}
+teams = {}
 
 def expected_score(team1_rating, team2_rating):
    expected_score = 1/(1+10**((team2_rating-team1_rating)/400))
@@ -12,17 +12,17 @@ def draw_probability(team1_rating, team2_rating):
     draw_prob = 0.3*math.exp(-elo_diff/400)
     return draw_prob
 
-def findWinner():
-     exp_score = expected_score(team1_rating = teams["France"], team2_rating=teams["England"])
-     draw_prob = draw_probability(team1_rating=teams["France"], team2_rating=teams["England"])
+def find_winner(team1, team2):
+     exp_score = expected_score(team1_rating = teams[team1], team2_rating=teams[team2])
+     draw_prob = draw_probability(team1_rating=teams[team1], team2_rating=teams[team2])
      team1_win_prob = exp_score - draw_prob/2
 
      r = random.random()
 
      if r < team1_win_prob:
-         result = "France Wins"
+         result = f'{team1} wins'
      elif r < team1_win_prob + draw_prob:
          result = "Draw"
      else: 
-        result = "England Wins"
+        result = f'{team2} wins'
      return result
