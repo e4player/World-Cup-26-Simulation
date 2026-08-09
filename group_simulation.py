2c77e913e4b0358a13987c1e1fc575f019cbb7a9
e4player <111000957+e4player@users.noreply.github.com>
2026-08-08 23:50:43 -0400

    added group creation and simulation logic

---
+++ group_simulation.py
@@ -0,0 +1,27 @@
+import one_match_simulation
+
+def create_group(teams):
+    groups = {}
+    for team in teams:
+        group = team["group"]
+        groups.setdefault(group, []).append(team)
+    return groups
+
+def update_standings(result, team1_stats, team2_stats):
+    if result == "team1 wins":
+        team1_stats['points'] += 1
+    elif result == "team2 wins":
+        team2_stats['points'] += 1
+    else:
+        team1_stats['points'] += 0.5
+        team2_stats['points'] += 0.5
+
+
+def simulate_group(groups):
+    for group_name, teams_info in groups.items():
+       for i in range(3):
+            for j in range(i+1,4):
+                  team1_stats = teams_info[i]
+                  team2_stats = teams_info[j]
+                  result = one_match_simulation.find_winner(team1_stats, team2_stats)
+                  update_standings(result, team1_stats, team2_stats)
