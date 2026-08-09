53d1278e1fb864d140483bcef98b5ddc6b5fdf33
e4player <111000957+e4player@users.noreply.github.com>
2026-08-08 23:41:09 -0400

    adapted find_winnersyntax to fit team_ratings json

--- one_match_simulation.py
+++ one_match_simulation.py
@@ -10,17 +10,17 @@ def draw_probability(team1_rating, team2_rating):
     draw_prob = 0.3*math.exp(-elo_diff/400)
     return draw_prob
 
-def find_winner(team1, team2, teams):
-     exp_score = expected_score(team1_rating = teams[team1], team2_rating=teams[team2])
-     draw_prob = draw_probability(team1_rating=teams[team1], team2_rating=teams[team2])
+def find_winner(team1_stats, team2_stats):
+     exp_score = expected_score(team1_stats['elo'], team2_stats['elo'])
+     draw_prob = draw_probability(team1_stats['elo'], team2_stats['elo'])
      team1_win_prob = exp_score - draw_prob/2
 
      r = random.random()
 
      if r < team1_win_prob:
-         result = f'{team1} wins'
+         result = "team1 wins"
      elif r < team1_win_prob + draw_prob:
          result = "Draw"
      else: 
-        result = f'{team2} wins'
+        result = "team2 wins"
      return result
