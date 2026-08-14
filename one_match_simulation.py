34ab40e787957135edc2ea56327b8cd95088c062
e4player <111000957+e4player@users.noreply.github.com>
2026-08-13 20:38:21 -0400

    added match simulation with no draws

--- one_match_simulation.py
+++ one_match_simulation.py
@@ -10,7 +10,7 @@ def draw_probability(team1_rating, team2_rating):
     draw_prob = 0.3*math.exp(-elo_diff/400)
     return draw_prob
 
-def find_winner(team1_stats, team2_stats):
+def get_result(team1_stats, team2_stats):
      exp_score = expected_score(team1_stats['elo'], team2_stats['elo'])
      draw_prob = draw_probability(team1_stats['elo'], team2_stats['elo'])
      team1_win_prob = exp_score - draw_prob/2
@@ -24,3 +24,14 @@ def find_winner(team1_stats, team2_stats):
      else: 
         result = "team2 wins"
      return result
+
+def find_winner(team1_stats, team2_stats):
+    exp_score = expected_score(team1_stats['elo'], team2_stats['elo'])
+
+    r = random.random()
+
+    if r < exp_score:
+        result = "team1 wins"
+    else:
+        result = "team2 wins"
+    return result
